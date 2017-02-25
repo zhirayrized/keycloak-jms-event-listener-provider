d32b820c29eb95630a7ed44e025457fdb34204b0
Zhirayr Kazarosyan <zhkazarosyan@gmail.com>
2017-02-25 22:51:15 +0300

    JSON message and separate queues for basic and admin events

---
+++ src/main/java/com/github/zhirayrized/keycloak/events/jms/EventProducer.java
@@ -0,0 +1,61 @@
+package com.github.zhirayrized.keycloak.events.jms;
+
+import com.google.gson.Gson;
+import org.keycloak.events.Event;
+import org.keycloak.events.admin.AdminEvent;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Zhirayr Kazarosyan
+ */
+public class EventProducer {
+    private final TopicConnectionFactory connectionFactory;
+
+    private final Topic eventDestinationTopic;
+
+    private final Topic adminEventDestinationTopic;
+
+    public EventProducer() {
+        try {
+            Context ctx = new InitialContext();
+            this.connectionFactory = (TopicConnectionFactory) ctx.lookup("java:/jms/KeycloakBusConnectionFactory");
+            this.eventDestinationTopic = (Topic) ctx.lookup("java:/jms/topic/KeycloakEvents");
+            this.adminEventDestinationTopic = (Topic) ctx.lookup("java:/jms/topic/KeycloakAdminEvents");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static EventProducer getInstance() {
+        return InstanceHolder.instance;
+    }
+
+    public void sendEvent(final Event event) {
+        sendEvent(event, eventDestinationTopic);
+    }
+
+    public void sendEvent(final AdminEvent event) {
+        sendEvent(event, adminEventDestinationTopic);
+    }
+
+    private void sendEvent(final Object payload, Topic topic) {
+        try (Connection connection = connectionFactory.createConnection();
+             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+
+            final Message message = session.createMessage();
+            message.setStringProperty("MEDIA_TYPE", "application/json");
+            message.setStringProperty("BODY", new Gson().toJson(payload));
+            session.createProducer(topic).send(message);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class InstanceHolder {
+        private final static EventProducer instance = new EventProducer();
+    }
+}
--- src/main/java/com/github/zhirayrized/keycloak/events/jms/KeycloakJMSEventListenerProviderFactory.java
+++ src/main/java/com/github/zhirayrized/keycloak/events/jms/KeycloakJMSEventListenerProviderFactory.java
@@ -1,24 +1,4 @@
-/*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates
- * and other contributors as indicated by the @author tags.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.github.zhirayrized.keycloak.events.jms;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
@@ -27,20 +7,23 @@ import org.keycloak.events.EventType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * @author Juraci Paixão Kröhling
  * @author Zhirayr Kazarosyan
  */
 public class KeycloakJMSEventListenerProviderFactory implements EventListenerProviderFactory {
     private final Set<EventType> excludedEvents = new HashSet<>();
 
     @Override
-    public EventListenerProvider create(KeycloakSession keycloakSession) {
+    public EventListenerProvider create(final KeycloakSession keycloakSession) {
         return new KeycloakJMSEventListenerProvider(Collections.unmodifiableSet(excludedEvents));
     }
 
     @Override
-    public void init(Config.Scope config) {
+    public void init(final Config.Scope config) {
         String[] excludes = config.getArray("excludes");
         if (excludes != null) {
             for (String e : excludes) {
@@ -59,6 +42,6 @@ public class KeycloakJMSEventListenerProviderFactory implements EventListenerPro
 
     @Override
     public String getId() {
-        return "keycloak-jms";
+        return "jms";
     }
 }
